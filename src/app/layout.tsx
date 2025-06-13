264b5553752b830dc81f185bf2532aa722f296ca
SayyedMohdZuhaib <zuhaibabaan@gmail.com>
2025-06-13 23:45:17 +0530

    Intial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-
-
-// import { dark } from '@clerk/themes'; // You can set a default baseTheme for all Clerk components here
-
-import Header from "@/components/header"; // Import your new Header component
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,16 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <html lang="en" suppressHydrationWarning>
-        <body className={`${inter.className} flex flex-col min-h-screen`}> {/* Added flex classes */}
-          <Header /> {/* Add the Header here */}
-          <main className="flex-grow"> {/* Make content grow to push footer down */}
-            {children}
-          </main>
-          {/* You might want a Footer component here too */}
-          {/* <Footer /> */}
-          <Toaster />
-        </body>
-      </html>
+    <html lang="en">
+      <body suppressHydrationWarning className={`${inter.className} flex flex-col min-h-screen`}>
+        <Header />
+        <main className="flex-grow">
+          {children}
+        </main>
+        {/* <Footer /> Uncomment if needed */}
+        <Toaster />
+      </body>
+    </html>
   );
 }
